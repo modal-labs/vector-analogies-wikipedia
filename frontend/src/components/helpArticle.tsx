1bf2462a039c2159064bf08cadd86ef0139e2b94
Charles Frye <charles@modal.com>
2024-05-03 13:17:47 -0700

    fix fmt error in helpArticle

--- frontend/src/components/helpArticle.tsx
+++ frontend/src/components/helpArticle.tsx
@@ -5,7 +5,7 @@ const helpArticle: Article = {
   content:
     "This is a 'vector analogy' search engine. Use the search bars to find Wikipedia snippets to construct three of the four parts of an analogy, like 'What is to D as A is to B?'." +
     " The engine then uses the vector representations of the snippets to try to complete the analogy." +
-    "Try 'What is to X as Albert Einstein is to Physics?' to learn about some amazingly accomplished people.",
+    " Try 'What is to X as Albert Einstein is to Physics?' to learn about some amazingly accomplished people.",
   identifier: -1,
   vector: [0.0],
   url: "https://github.com/charlesfrye/vector-analogies-wikipedia",
