8f3b88cd822f3cfc155ce517c05b3160cf5c03a2
Charles Frye <charles@modal.com>
2024-05-03 16:39:23 -0700

    dev backend support

--- frontend/src/services/weaviateService.ts
+++ frontend/src/services/weaviateService.ts
@@ -4,11 +4,16 @@ interface Results {
   results: Article[];
 }
 
+const urlStem = `https://${
+  import.meta.env.VITE_MODAL_WORKSPACE
+}--modal-weaviate`;
+
+const urlSuffix =
+  import.meta.env.VITE_DEV_BACKEND === "true" ? "-dev.modal.run" : `.modal.run`;
+
 const searchArticles = async (searchText: string) => {
   const response = await fetch(
-    `https://${
-      import.meta.env.VITE_MODAL_WORKSPACE
-    }--modal-weaviate-query.modal.run?q=${encodeURIComponent(searchText)}`
+    `${urlStem}-query${urlSuffix}?q=${encodeURIComponent(searchText)}`
   );
   const data: Results = await response.json();
   return data["results"];
@@ -18,18 +23,13 @@ const getNearest = async (vector: number[]) => {
   const inData = {
     vector: vector,
   };
-  const response = await fetch(
-    `https://${
-      import.meta.env.VITE_MODAL_WORKSPACE
-    }--modal-weaviate-vector.modal.run`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(inData),
-    }
-  );
+  const response = await fetch(`${urlStem}-vector${urlSuffix}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(inData),
+  });
   const data: Results = await response.json();
   return data["results"][0];
 };
